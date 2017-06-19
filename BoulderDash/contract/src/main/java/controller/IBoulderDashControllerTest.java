90bee3bcf4acbb51612292e5fac97ab1cb558f76
Tejesh Tailor <Tejesh Tailor@10.192.128.46>
2017-06-19 15:06:10 +0200

    Test JUnit

---
+++ BoulderDash/contract/src/main/java/controller/IBoulderDashControllerTest.java
@@ -0,0 +1,39 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class IBoulderDashControllerTest {
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void testStart() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetOrderPeformer() {
+    fail("Not yet implemented");
+  }
+
+}
