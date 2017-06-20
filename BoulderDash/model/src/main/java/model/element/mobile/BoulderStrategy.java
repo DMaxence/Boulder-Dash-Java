286344fee396445a22e1ac4fe9762cb110b87fef
Paul <abonnementspaul@gmail.com>
2017-06-20 09:39:40 +0200

    moved files

--- BoulderDash/model/src/main/java/model/element/mobile/BoulderStrategy.java
+++ BoulderDash/model/src/main/java/model/element/mobile/BoulderStrategy.java
@@ -6,7 +6,7 @@ import model.IMap;
 public class BoulderStrategy extends FallingObjectsStrategy {
 
 	@Override
-	public void followStrategy(IMobile currentPawn, IMap map, IMobile myCharacter) {
-		super.followStrategy(currentPawn, map, myCharacter);
+	public void followStrategy(IMobile currentPawn, IMap map) {
+		super.followStrategy(currentPawn, map);
 	}
 }
