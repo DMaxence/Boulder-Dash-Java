ba6c8f2993606c1ff756f236a49c9f784e0acf93
Tejesh Tailor <Tejesh Tailor@10.192.128.46>
2017-06-20 09:46:09 +0200

    javadoc modification

--- BoulderDash/contract/src/main/java/controller/package-info.java
+++ BoulderDash/contract/src/main/java/controller/package-info.java
@@ -1,7 +1,6 @@
 /**
- * Provides all contracts for the controller component.
- *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
+ * Provides all contracts for the controller component
+ * @author Tejesh Tailor
+ * 
  */
 package controller;
