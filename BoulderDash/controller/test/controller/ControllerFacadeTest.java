adede58e1614e81646600e038c9a94ff341cad98
Tejesh Tailor <Tejesh Tailor@10.192.128.46>
2017-06-21 12:46:01 +0200

    test junit add for module controller

---
+++ BoulderDash/controller/test/controller/ControllerFacadeTest.java
@@ -0,0 +1,39 @@
+package controller;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import model.IModel;
+
+
+public class ControllerFacadeTest {
+   IModel model;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+
+  @Test
+  public void testGetOrderPeformer() {
+    assertNotNull(this);
+  }
+
+
+}
