5decd1ee35bbe699825a1a10e2ff620a433a4e2c
Tejesh Tailor <Tejesh Tailor@10.192.128.46>
2017-06-21 13:40:33 +0200

    junit test suite element

---
+++ BoulderDash/model/test/element/ElementTest.java
@@ -0,0 +1,59 @@
+package element;
+
+import static org.junit.Assert.*;
+
+import java.awt.Rectangle;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import model.element.Permeability;
+import model.element.Sprite;
+
+public class ElementTest {
+  private Sprite sprite;
+  private Permeability permeability = Permeability.PENETRABLE;
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    sprite = new Sprite('T', Sprite.characterTileSet, new Rectangle(1,93,27,92));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+
+  @Test
+  public void testGetSprite() {
+    assertNotNull(this.sprite);
+    assertNotNull(this.sprite.getConsoleImage());
+    
+  }
+
+  @Test
+  public void testGetPermeability() {
+    assertNotNull(this.permeability);
+  }
+
+  @Test
+  public void testGetImage() {
+    /**
+     * As we don't load the buffer, we don't have image to load
+     * That why we have a null result
+     */
+    assertNull(this.sprite.getImage());
+  }
+
+}
